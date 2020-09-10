ea5be5d8a7b0fd9a6f939593d4a29abaebcdb468
Barraki <Barraki201@gmail.com>
2020-09-11 01:54:17 +0300

    add desktop adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 import './vendor/focus-visible.min.js';
 import './_vars';
-import {resizeContent} from './functions/resize';
-import {scrollTo} from './functions/smooth-scroll';
-import {disableScroll, enableScroll} from './functions/stop-scroll';
+import { resizeContent } from './functions/resize';
+import { scrollTo } from './functions/smooth-scroll';
+import { disableScroll, enableScroll } from './functions/stop-scroll';
 
 //disableScroll(fix) // fix -> class of element with position: fixed
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+
+anchors.forEach((el) => {
+  el.addEventListener('click', (e) => {
+      e.preventDefault();
+      let id = e.currentTarget.getAttribute('href');
+      scrollTo(document.querySelector(id))
+  });
+})
