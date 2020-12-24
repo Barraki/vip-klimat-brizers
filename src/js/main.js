d9c1e9f791d6c373ed5c1816e5091e60544b5506
Barraki <Barraki201@gmail.com>
2020-12-24 02:53:59 +0200

    Add modal forms and popups

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
   var topOffset = 0;
   AOS.init();
 
-/*================== Preloader start ==================*/
+  /*================== Preloader start ==================*/
   $(window).on('load', function () {
     $(".loader").fadeOut();
     $("#preloder").delay(200).fadeOut("slow");
@@ -36,16 +36,19 @@
   /*================== Slider testimonials end ==================*/
 
   /*================== Anchor testimonials start ==================*/
-  if (widthWindow < 768) {
-    topOffset = 78;
-  }
-
-  $('a[href^="#"]').on("click", function () {
-    var target = $(this).attr("href");
-    if (target && target !== '#') {
-      $("html, body").animate({ scrollTop: $(target).offset().top - topOffset }, 1000);
+  $(function () {
+    if (widthWindow < 768) {
+      topOffset = 78;
     }
-    return false;
+
+    $('a[href^="#"]').on("click", function () {
+      var target = $(this).attr("href");
+
+      if (target && target !== '#') {
+        $("html, body").animate({ scrollTop: $(target).offset().top - topOffset }, 1000);
+      }
+      return false;
+    });
   });
   /*================== Anchor testimonials end ==================*/
 
@@ -67,21 +70,77 @@
   /*================== Canvas Menu end ==================*/
 
   /*================== Modal window start ==================*/
-  $(function () {
-    // (".open-popup-link").magnificPopup({
-    //   type: 'inline',
-    //   midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-    // });
+
+  $('.modal-link').on("click", function (event) {
+    event.preventDefault();
+    this.blur();
+    var target = $(this).attr("href");
+    if (target && target !== "#") {
+      $.get(target, function () {
+        $(this.url).modal({
+          fadeDuration: 100,
+          fadeDelay: 0.30,
+        });
+      });
+    }
   });
 
-  // TODO!: Add modal windows
   /*================== Modal window end ==================*/
 
-  /*================== Modal window start ==================*/
+  /*================== Mask phone start ==================*/
 
-  $('input[name=phone]').mask("+38 (000) 000-00-00");
+  $('input[name=phone]').mask("+7-000-000-00-00");
 
-  /*================== Modal window end ==================*/
+  /*================== Mask phone end ==================*/
 
+  /*================== Form handler start ==================*/
+
+  $('.send_request_to_spec').submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "/backend/mail.php", //Change
+      data: th.serialize(),
+      beforeSend: function () {
+        $('.popup-wrapper').show();
+        $('.modal .close').click()
+      }
+    }).done(function () {
+      $('.popup-loading img').css("display", "none")
+      $('.popup-loading h3 ').show();
+      $('.popup-loading h2 ').text('Готово');
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    }).fail(function () {
+      alert("Ошибка, попробуйте позже");
+    });
+    return false;
+  });
+
+  /*================== Form handler start ==================*/
+
+  /*================== Catch url params start ==================*/
+
+  $(function () {
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
+
+    var utm_source = !urlParams.get('utm_source') == '' ? urlParams.get('utm_source') : '';
+    var utm_medium = !urlParams.get('utm_medium') == '' ? urlParams.get('utm_medium') : '';
+    var utm_campaign = !urlParams.get('utm_campaign') == '' ? urlParams.get('utm_campaign') : '';
+    var utm_content = !urlParams.get('utm_content') == '' ? urlParams.get('utm_content') : '';
+    var utm_term = !urlParams.get('utm_term') == '' ? urlParams.get('utm_term') : '';
+
+
+    !urlParams.get('utm_source') == '' ? $('form').append('<input type="hidden" name="utm_source" value="' + utm_source + '">') : '';
+    !urlParams.get('utm_medium') == '' ? $('form').append('<input type="hidden" name="utm_medium" value="' + utm_medium + '">') : '';
+    !urlParams.get('utm_campaign') == '' ? $('form').append('<input type="hidden" name="utm_campaign" value="' + utm_campaign + '">') : '';
+    !urlParams.get('utm_content') == '' ? $('form').append('<input type="hidden" name="utm_content" value="' + utm_content + '">') : '';
+    !urlParams.get('utm_term') == '' ? $('form').append('<input type="hidden" name="utm_term" value="' + utm_term + '">') : '';
+  });
+
+  /*================== Catch url params end ==================*/
 
 })($, jQuery);
